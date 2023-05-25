a469888752a974a73ca15ca26988e1a89cf0726e
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-05-25 16:22:24 +0200

    Initial commit

--- Program/src/FizzBuzz.java
+++ Program/src/FizzBuzz.java
@@ -7,6 +7,7 @@ public class FizzBuzz {
         // 3 % 3 = 0 && 5 % 5 = 0 FizzBuzz
 
         for (int i = 1; i <= 100; i++) {
+            // Vi kontrollerar först ifall båda stämmer, så att vi skriver ut fizzBuzz. Hade den varit underst, så hade vi printat "Fizz \n Buzz", vilket är fel.
             if (i % 5 == 0 && i % 3 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
